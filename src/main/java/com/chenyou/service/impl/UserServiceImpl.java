20c629bbe8242dff41ac6225e0638ce307b7abc2
haoqwer <haowenjie7173@163.com>
2018-11-24 18:28:12 +0800

    新增用户增加渠道id

--- src/main/java/com/chenyou/service/impl/UserServiceImpl.java
+++ src/main/java/com/chenyou/service/impl/UserServiceImpl.java
@@ -6,6 +6,7 @@ import com.chenyou.mapper.UserMapper;
 import com.chenyou.pojo.User;
 import com.chenyou.pojo.entity.PageResult;
 import com.chenyou.service.UserService;
+import com.chenyou.utils.MD5Utils;
 import com.chenyou.utils.StringUtils;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
@@ -37,6 +38,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public int saveUser(User user) throws BizException {
         int i;
+        String channelIds = "";
         if (StringUtils.isNull(user)) {
             throw new BizException(BizException.CODE_PARM_LACK, "新增用户信息不能为空!");
         }
@@ -54,10 +56,18 @@ public class UserServiceImpl implements UserService {
             throw new BizException(BizException.CODE_PARM_LACK, "请输入渠道名称!");
         }
         checkChannelIdUnique(user);
-        if (StringUtils.isEmpty(user.getChannelId())) {
+        if (StringUtils.isEmpty(user.getChannelIds())) {
             throw new BizException(BizException.CODE_PARM_LACK, "请输入渠道号!");
         }
+        //增加用户
+        for (int j = 0; j < user.getChannelIds().length; j++) {
+            channelIds = channelIds + user.getChannelIds()[j] + ",";
+        }
+        user.setChannelId(channelIds);
         user.setCreateTime(new Date());
+        user.setEnable(1);
+        user.setPassword(MD5Utils.md5(user.getPassword()));
+        user.setRoleName("渠道");
         i = userMapper.insertSelective(user);
         return i;
     }
@@ -71,8 +81,8 @@ public class UserServiceImpl implements UserService {
     public String checkChannelIdUnique(User user) throws BizException {
         int id = null == user.getId() ? -1 : user.getId();
         User oldUser = userMapper.checkChannelIdUnique(user.getChannelId());
-        if(StringUtils.isNotNull(oldUser) && id != oldUser.getId()){
-            throw new BizException(BizException.CODE_PARM_LACK,"渠道ID"+user.getChannelId()+"已经存在!");
+        if (StringUtils.isNotNull(oldUser) && id != oldUser.getId()) {
+            throw new BizException(BizException.CODE_PARM_LACK, "渠道ID" + user.getChannelId() + "已经存在!");
         }
         return UserConstants.USER_PHONE_UNIQUE;
     }
@@ -81,20 +91,38 @@ public class UserServiceImpl implements UserService {
     public String checkLoginNameUnique(User user) throws BizException {
         int id = null == user.getId() ? -1 : user.getId();
         User oldUser = userMapper.checkLoginNameUnique(user.getLoginName());
-        if(StringUtils.isNotNull(oldUser) && id != oldUser.getId()){
-            throw new BizException(BizException.CODE_PARM_LACK,"用户名"+user.getLoginName()+"已经存在!");
+        if (StringUtils.isNotNull(oldUser) && id != oldUser.getId()) {
+            throw new BizException(BizException.CODE_PARM_LACK, "用户名" + user.getLoginName() + "已经存在!");
         }
         return UserConstants.USER_PHONE_UNIQUE;
     }
 
     @Override
     public PageResult findPage(int pageNum, int PageSize) throws BizException {
-        PageHelper.startPage(pageNum,PageSize);
+        PageHelper.startPage(pageNum, PageSize);
         List <User> list = userMapper.selectByExample(null);
-        if(StringUtils.isEmpty(list)){
-            throw new BizException(BizException.CODE_PARM_LACK,"不好意思当前没有数据!");
+        if (StringUtils.isEmpty(list)) {
+            throw new BizException(BizException.CODE_PARM_LACK, "不好意思当前没有数据!");
         }
-        Page<User> page=(Page<User>)list;
-        return new PageResult(page.getTotal(),page.getResult());
+        Page <User> page = (Page <User>) list;
+        return new PageResult(page.getTotal(), page.getResult());
+    }
+
+    @Override
+    public User findOne(Integer id) {
+        return userMapper.selectByPrimaryKey(id);
+    }
+
+    
+    
+    @Override
+    public int addChannelId(User user) throws BizException {
+        //获取到之前的channelId
+        User oldUser = userMapper.selectByPrimaryKey(user.getId());
+        String oldChannelId = oldUser.getChannelId();
+        if(StringUtils.isEmpty(user.getChannelIds())){
+            throw new BizException(BizException.CODE_PARM_LACK,"请输入新增渠道ID");
+        }
+        return 0;
     }
 }
